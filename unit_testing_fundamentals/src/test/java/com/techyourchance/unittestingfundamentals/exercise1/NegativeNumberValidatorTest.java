9d9115d8514167852ea92c4e3d7215cfac9db1f9
Glenn Ross Pascual <glenn.development@gmail.com>
2019-08-13 22:04:19 +0800

    Added NegativeValidatorTest and fixed NegativeValidator's functionality to meet its requirements.

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,39 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setUpSUT() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void testPositive() {
+        boolean negative = SUT.isNegative(1);
+        Assert.assertThat(negative, Is.is(false));
+    }
+
+    @Test
+    public void testZero() {
+        boolean negative = SUT.isNegative(0);
+        Assert.assertThat(negative, Is.is(false));
+    }
+
+    @Test
+    public void testNegative() {
+        boolean negative = SUT.isNegative(-1);
+        Assert.assertThat(negative, Is.is(true));
+    }
+
 
 }
